7e5c03a8a9445a143af03b12ce00614152835e3d
PC Tuto <PC Tuto@PCTuto-PC>
2014-04-13 23:10:06 -0300

    Commit 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -6,10 +7,14 @@ public class Main {
 	public static void main(String[] args){
 		JFrame frame = new JFrame();
 		JLabel label = new JLabel();
+		JButton button = new JButton();
 		label.setText("AQUI DEBIESE IR UN TEXTO MUY INTERESANTE");
 		frame.add(label);
+		frame.add(button);
+	    button.setText("PRESIONAME");
 		frame.setTitle("Mi primera ventana");
-		frame.setSize(1240,600);
+		frame.setSize(800,600);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		     frame.setVisible(true);
 	}
